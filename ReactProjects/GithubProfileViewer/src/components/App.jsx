44f563746bf8cfb83012e6363a72769198d94466
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-08-20 21:53:01 +0200

    Finishing the GithubProfilViewer Projects, adding the also a Search bar

--- ReactProjects/GithubProfileViewer/src/components/App.jsx
+++ ReactProjects/GithubProfileViewer/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Profile from './github/Profile.jsx';
+import Search from './github/Search.jsx';
 
 class App extends Component {
 	constructor(props) {
@@ -46,6 +47,13 @@ class App extends Component {
 		});
 	}
 
+	handleFormSubmit(username) {
+		this.setState({username: username}, function() {
+			this.getUserData();  
+	    	this.getUserRepos();
+		});
+	}
+
 	
 
 	componentDidMount() {
@@ -56,6 +64,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
+				<Search onFormSubmit = {this.handleFormSubmit.bind(this)}/>
 				<Profile {...this.state} />
 			</div>
 		)
