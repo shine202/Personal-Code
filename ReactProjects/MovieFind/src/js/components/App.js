bd8311a973136cbdd70d9568ae3e54f52960763e
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-08-22 20:32:10 +0200

    Working on the Movie Finder Files, on Main.js and App.js

---
+++ ReactProjects/MovieFind/src/js/components/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+var App = React.createClass({
+    render: function() {
+        return(
+            <div>
+                MY APP
+            </div>
+        )
+    }
+});
+
+module.exports = App;
