c2faac844953a6ee3d15267bc9f8bdc46ce25fc9
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-08-31 10:26:19 +0200

    removing the yelp folder, not a well suited tutorial

--- ReactProjects/yelp/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const webapack = require('webapack');
-const fs       = require('fs');
-const path     = require('path'),
-      join     = path.join,
-      resolve  = path.resolve;
-
-
-const getConfig = require('hjs-webapack');
