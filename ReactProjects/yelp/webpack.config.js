d5c3efb3cbfb870f22df92b15f55fb6e41b30fc5
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-08-31 09:32:50 +0200

    Creating the Basic Layouts for the Yelp React Clone Tutorial

---
+++ ReactProjects/yelp/webpack.config.js
@@ -0,0 +1,8 @@
+const webapack = require('webapack');
+const fs       = require('fs');
+const path     = require('path'),
+      join     = path.join,
+      resolve  = path.resolve;
+
+
+const getConfig = require('hjs-webapack');
