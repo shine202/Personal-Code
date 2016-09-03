7cff14cd4a3ca0c34d641a0489c0a1ab313cbaf2
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-09-03 19:18:58 +0200

    Working on the ReactInterface

--- ReactProjects/ReactInterface/process/js/app.js
+++ ReactProjects/ReactInterface/process/js/app.js
@@ -1,2 +1,61 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 
+var MainInterface = React.createClass({
+    getInitialState: function() {
+        return {
+            data: [
+              {
+                "petName": "Rocku",
+                "ownerName": "Hassum Harrod",
+                "aptDate": "2016-06-20 15:30",
+                "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
+              },
+              {
+                "petName": "Spot",
+                "ownerName": "Constance Smith",
+                "aptDate": "2016-06-24 08:30",
+                "aptNotes": "This German Shepherd is having some back pain"
+              },
+              {
+                "petName": "Goldie",
+                "ownerName": "Barot Bellingham",
+                "aptDate": "2016-06-22 15:50",
+                "aptNotes": "This Goldfish has some weird spots in the belly"
+              },
+              {
+                "petName": "Mitten",
+                "ownerName": "Hillary Goldwyn",
+                "aptDate": "2016-06-21 9:15",
+                "aptNotes": "Cat has excessive hairballs"
+              }
+            ]
+        }
+    },
+
+    render: function() {
+
+        return (
+            <div className="interface">
+                <ul className="item-list media-list">
+                    <li className="pet-info media-body">
+                        <div className="pet-head">
+                            <span className="pet-name">{this.state.data[0].petName}</span>
+                            <span className="apt-date pull-right">{this.state.data[0].aptDate}</span>
+                        </div>
+                        <div className="owner-name"><span className="label-item>Owner: </span>
+                            {this.state.data[0].ownerName}
+                        </div>
+                        <div className="apt-notes">{this.state.data[0].aptNotes}</div>
+                    </li>
+                </ul>
+           </div>
+        )
+    }
+});
+
+
+ReactDOM.render(
+    <MainInterface />,
+    document.getElementById('petAppointments')
+);
