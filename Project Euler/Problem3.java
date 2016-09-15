f565771d0186b7262ab99fefe5feb2528a035ba9
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-09-15 12:00:29 +0200

    Adding the third solution

---
+++ Project Euler/Problem3.java
@@ -0,0 +1,30 @@
+
+
+public class Problem3 {
+    public static void main(String[] args) {
+        long n= 600851475143L; //this is a large prime number 
+        long i = 2L;
+        int test = 0;
+
+        while (n > 1)
+        {
+            while (n % i == 0)
+            {
+                n /= i;     
+            }
+
+            i++;
+
+            if(i*i > n && n > 1) 
+            {
+                System.out.println(n); //prints n if it's prime
+                test = 1;
+                break;
+            }
+        }
+
+        if (test == 0)  
+            System.out.println(i-1); //prints n if it's the largest prime factor
+    }
+}
+    
