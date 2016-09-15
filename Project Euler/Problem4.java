72b5dd62279e3f8526881401b5640aacd805cd69
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-09-15 13:52:32 +0200

    Adding the fourth problem

---
+++ Project Euler/Problem4.java
@@ -0,0 +1,35 @@
+
+public class Problem4 {
+
+    public static void main(String [] arg) {
+
+        Problem4 problem4 = new Problem4();
+        
+        long biggest = 0;
+
+        int a = 999, b = 999;
+
+        for (int bb = b; bb > 0; bb--) {
+            for (int aa = a; aa > 0; aa--) {
+                if ( problem4.isPalindrome( new Long(aa*bb).toString())) {
+                    if ( aa*bb > biggest ) {
+                        biggest = aa*bb;
+                    }
+                }
+            }
+        }
+
+        System.out.println(biggest);   
+    }
+
+    public boolean isPalindrome( String s ) {
+        //if odd length then remove middle character
+        if ( s.length() % 2 != 0 ) {
+        
+            s = s.substring( 0, s.length() / 2 ) + s.substring( (s.length() / 2) + 1);
+        }
+
+        return new StringBuffer( s.substring( 0, s.length() / 2) ).
+                                reverse().toString().equals( s.substring( s.length() / 2) );
+    }
+}
