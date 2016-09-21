3851e419c1719587bff685db16255016c958ccd3
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-09-21 09:57:01 +0200

    Adding the 17th Solution

---
+++ Project Euler/Euler17.java
@@ -0,0 +1,63 @@
+
+public class Euler17 {
+
+    public static String[] ones = {
+       "","one","two","three","four","five","six","seven","eight","nine","ten",
+      "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen" 
+    };
+
+    public static String[] tens = {
+        "","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"
+    };
+
+    public static String[] hundreds = {
+        "","onehundred","twohundred","threehundred","fourhundred","fivehundred","sixhundred","sevenhundred","eighthundred","ninehundred"
+    };
+
+    public String word(int arg) {
+        String withAnd = "";
+
+        if(arg == 1000) {
+            return "onethousand";
+        } 
+
+        if(arg < 20) {
+            return ones[arg];
+        }
+        if(arg < 100) {
+            return tens[arg/10] + word(arg%10);
+        }
+
+        withAnd = word(arg % 100);
+
+        if(withAnd.length() > 0) {
+            withAnd = "and" + withAnd;
+        }
+
+        return hundreds[arg/100] + withAnd;
+    }
+
+    public int letterCt(int arg) {
+        //System.out.println(word(arg));
+        //System.out.println(word(arg).length());
+        return word(arg).length();
+    }
+
+    public static void main(String [] arg) {
+        long start = System.currentTimeMillis();
+
+        int result = 0;
+
+        for(int i = 1; i <= 1000; i++) {
+            //System.out.println("numWord: "+i);
+            result += new Euler17().letterCt(i);
+        }
+
+        System.out.println("Result: "+result);
+
+
+        long stop = System.currentTimeMillis();
+        System.out.println(stop-start + "ms");
+    }
+
+}
