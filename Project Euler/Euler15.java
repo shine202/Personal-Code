3270220519336fc523a0957739b152e53d215eb7
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-09-21 08:50:22 +0200

    Adding the 15th Solution

---
+++ Project Euler/Euler15.java
@@ -0,0 +1,44 @@
+
+public class Euler15 {
+
+    public static long binomialCoefficient(int n, int k) {
+		/* N-choose-k combinatorics: (n! / (k! * (n-k)!)
+		 * Where:
+		 * 		n is the number of moves,
+		 * 		k is the number of down and right moves required (20 each) */
+ 
+		if (k > (n-k))
+			k = n - k;
+		
+		long c = 1;
+		
+		for (int i = 0; i < k; i++)
+		{
+			c = c * (n-i);
+			c = c / (i+1);			
+		}
+		
+		return c;
+        
+	}
+
+    public static long factorial(int n) {
+        long sum  = 1;
+        for(int i = 1; i<=n; i++) {
+            sum *= i;
+        }
+        return sum;
+    }
+
+    public static void main(String [] arg) {
+
+        long begin = System.currentTimeMillis();
+		
+		System.out.println(binomialCoefficient(40,20));
+		
+		long end = System.currentTimeMillis();
+		System.out.println(end-begin + "ms");	
+    }
+
+    
+}
