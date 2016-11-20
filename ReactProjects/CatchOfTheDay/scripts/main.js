807570e73bda93cbd91bf1c87de7b20349a0718c
Shkelqim1410 <shkelqim.mula1410@gmail.com>
2016-11-20 14:36:03 +0100

    Adding the Helpers and Mixins

--- ReactProjects/CatchOfTheDay/scripts/main.js
+++ ReactProjects/CatchOfTheDay/scripts/main.js
@@ -5,8 +5,12 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
+var NotFoundRoute = ReactRouter.NotFoundRoute;
 
 var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+var h = require('./helpers');
 
 /*
 	App
@@ -78,11 +82,20 @@ var Inventory = React.createClass({
 */
 var StorePicker = React.createClass({
 
+	mixins: [History],
+	goToStore: function(event) {
+		event.preventDefault();
+		// get the data from the input
+		var storeId = this.refs.storeId.value;
+		this.history.pushState(null, '/store/'+ storeId);
+		// transition from <StorePicker /> to <App />
+
+	},
 	render: function() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter a Store</h2>
-				<input type="text" ref="storeId" required />
+				<input type="text" ref="storeId" defaultValue={h.getFunName()} required />
 				<input type="Submit" />
 			</form>
 		)
